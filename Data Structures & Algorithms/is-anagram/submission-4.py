622bcfbe280ccb1ad015462cd437c01a72cc3b0d
HARSHINI M <123100009+HarshiniMuthukaruppan@users.noreply.github.com>
2026-04-25 18:29:04 +0530

    Add: is-anagram - submission-4

---
+++ Data Structures & Algorithms/is-anagram/submission-4.py
@@ -0,0 +1,22 @@
+class Solution:
+    def isAnagram(self, s: str, t: str) -> bool:
+        d={}
+        if len(s) !=len(t):
+            return False
+
+        for i in s:
+            if i in d:
+                d[i]+=1
+            else:
+                d[i]=1
+
+        for i in t:
+            if i in d and d[i]>0 :
+                d[i]-=1
+            else:
+                return False
+
+        return    True    
+               
+
+        
